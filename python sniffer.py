119cf6734dbab55eeb1fe90a1bfdfa4d7ec6bbf5
kazimdawood3-dev <kazimdawood3@gmail.com>
2026-05-01 11:47:38 +0500

    Add files via upload

---
+++ python sniffer.py
@@ -0,0 +1,12 @@
+from scapy.all import sniff
+
+def packet_callback(packet):
+    if packet.haslayer("IP"):
+        src_ip = packet["IP"].src
+        dst_ip = packet["IP"].dst
+        proto = packet["IP"].proto
+        print(f"Source: {src_ip} | Destination: {dst_ip} | Protocol: {proto}")
+
+# نیٹ ورک ٹریفک کو مانیٹر کرنا شروع کریں
+print("Sniffing started...")
+sniff(prn=packet_callback, store=0)
